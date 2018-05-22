4a24a85c698b8e00338035a6b4137533fd00d064
Hannah Ward <Hannah.ward9001@gmail.com>
2018-05-22 10:35:26 +0100

    fix: Compress digits to jpg

---
+++ digits/compress.sh
@@ -0,0 +1,5 @@
+for i in {0..12}                                                              [±master ●●]
+do
+    convert -quality 60 raw/$i.png $i.jpg
+done
+
--- digits/pack.py
+++ digits/pack.py
@@ -4,10 +4,10 @@ import json
 
 export = {}
 
-for f in glob.glob("*.png"):
+for f in glob.glob("*.jpg"):
     with open(f, "rb") as img:
         img64 = base64.b64encode(img.read())
-        export[f.replace(".png", "")] = "data:image/png;base64,{}".format(img64.decode())
+        export[f.replace(".jpg", "")] = "data:image/jpg;base64,{}".format(img64.decode())
 
 with open("digits.json", "w") as f:
     f.write(json.dumps(export))  
